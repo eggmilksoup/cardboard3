1f0b597c2dba562d76988e623230c4fb524f9aa8
Egg Milk Soup <egg@milksoup.net>
2022-08-14 09:09:06 -0500

    fix newline replacement

---
+++ scripts/findrule.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# findrule.sh version 3.0.0
+
+top=$(grep -n "^\\*\\* Rule $1" /var/git/tns-nomic.git/da-rules.org |
+	cut -f 1 -d : |
+	tr  -d '\n')
+
+bot=$(tail -n +$(($top + 1)) /var/git/tns-nomic.git/da-rules.org |
+	grep -n '\*\* Rule' |
+	cut -f 1 -d : |
+	head -n 1 |
+	tr -d '\n')
+
+tail -n +$top /var/git/tns-nomic.git/da-rules.org |
+	head -n $bot |
+	sed 's/\*/\\\*/g' |
+	sed 's/  / /' | 
+	tr '\n' ' ' |
+	sed 's/  /\
+\
+/g'
